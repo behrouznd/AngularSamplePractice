6d52ac00fc03f17ea06af78add5588191d0cd541
behrouz naderi <behrouz1986@gmail.com>
2021-12-20 13:48:20 +0330

    14000829

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls:['./user.component.css']
+//  styles: [`
+//p{color:red}
+//`]
+//  template: `
+//<p>Template : <\p>
+//`
+})
+
+export class UserComponent {
+
+}
